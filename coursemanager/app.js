3e6e6ed444cef9fd7fd09be1df99e264274ede71
Justin <justin.lail@hotmail.com>
2018-01-29 16:44:13 -0500

    testing remote changes

--- coursemanager/app.js
+++ coursemanager/app.js
@@ -38,8 +38,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
