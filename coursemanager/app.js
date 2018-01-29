6389bad89434b524a37373ac70f9d72a1828d81d
Justin <justin.lail@hotmail.com>
2018-01-29 16:17:40 -0500

    cors update

--- coursemanager/app.js
+++ coursemanager/app.js
@@ -20,7 +20,7 @@ mongoose.connect(mongoURL)
 	.catch(()=> {console.log('Error connecting to mongo at ' + mongoURL)})
 
 app.use(function(req, res, next){
-	res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+	res.header("Access-Control-Allow-Origin", "http://ec2-18-216-44-63.us-east-2.compute.amazonaws.com:3000");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	next();
